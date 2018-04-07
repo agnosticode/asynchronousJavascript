14e95e00a786ce21d7a6b61c6c85db0af8ac1979
Satinder Singh <agnosticode@gmail.com>
2018-04-07 21:24:34 +0530

    ES6 Promises

--- app.js
+++ app.js
@@ -3,34 +3,21 @@ const posts = [
     {title:'Post Two',body:'This is post two'}
 ]
 
-//DOING THINGS SYNCHRONOUSLY
-
-// function createPost(post){
-//     setTimeout(function(){
-//         posts.push(post);
-//     }, 2000);
-// }
-
-// function getPost(){
-//     setTimeout(function(){
-//         let output = '';
-//         posts.forEach(function(post){
-//             output += `<li>${post.title}</li>`
-//         });
-//         document.body.innerHTML=output; 
-//     },1000);
-// }
-
-// createPost({title:'Post Three', body:'This is post three'});    
-// getPost();
-
 //DOING THINGS ASYNCHRONOUSLY
-function createPost(post, callback){
-    setTimeout(function(){
-        posts.push(post);
-        callback();
-    }, 2000);
-}
+function createPost(post){
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            posts.push(post);
+            const error=false;
+            if(!error){
+                resolve();
+            }else{
+                reject('Error : Something went wrong')
+            }
+            resolve();
+        }, 2000);
+    });
+}   
 
 function getPost(){
     setTimeout(function(){
@@ -42,4 +29,6 @@ function getPost(){
     },1000);
 }
 
-createPost({title:'Post Three', body:'This is post three'},getPost); 
+createPost({title:'Post Three', body:'This is post three'}).then(getPost).catch(function(err){
+    console.log(err);
+}); 
